c298b5b7b107ddfebd9a01c9a5963234ae35759a
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-04-26 20:26:38 -0300

    pagina SOBRE terminada

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -4,9 +4,9 @@ const Sobre = () => {
   return (
     <div className='container container-sobre'>
       <div className='text-left'>
-        <h1>Olá! Meu nome é Lucas Ribeiro e eu sou...</h1>
+        <h1>Olá! Meu nome é Lucas Ribeiro...</h1>
         <p>
-          programador, ou como a maioria gosta de chamar, Desenvolvedor Frontend, tenho 18 anos, nasci em Piracicaba que fica localizado no interior de São Paulo.
+          e sou programador, ou como a maioria gosta de chamar, Desenvolvedor Frontend, tenho 18 anos, nasci em Piracicaba que fica localizado no interior de São Paulo.
         </p>
         <p>
           Desde de criança sempre gostei da área de TI e de mexer em computadores, depois dos 16 anos comecei a me interessar sobre o mundo da programação e então virou minha paixão.
@@ -15,7 +15,7 @@ const Sobre = () => {
           No meu dia a dia eu geralmente desenvolvo as minhas aplicações utilizando as seguintes ferramentas: Javascript, React.JS, Styled Components ou CSS padrão.
         </p>
         <p>
-          Eu sou o tipo de pessoa que sempre precisa estar aprendendo algo novo diariamente, se não meu dia não está feito, ultimamente estou estudando Next.JS.
+          Eu sempre estou aprendendo algo novo, ultimamente estou estudando Next.JS, para mais detalhes das minhas Habilidades Técnicas, acesse a sessão <strong>Skills</strong>.
         </p>
         <p>
           Compartilho dicas e conhecimentos sobre a área de programação frontend no meu Instagram - @Devcodepro.
@@ -24,9 +24,9 @@ const Sobre = () => {
       <div className='photo-right'>
         <img src="/assets/myPhoto.png" alt="Lucas ribeiro" />
         <ul>
-          <li><a href="">Veja meus projetos no Github.</a></li>
-          <li><a href="">Conheça meu Instagram.</a></li>
-          <li><a href="">Visite meu LinkedIn.</a></li>
+          <li><a href="https://github.com/devlucaspro"><i className="fa-brands fa-github"></i> Veja meus projetos no Github.</a></li>
+          <li><a href="https://www.instagram.com/devcodepro"><i className="fa-brands fa-instagram"></i> Conheça meu Instagram.</a></li>
+          <li><a href="https://www.linkedin.com/in/devlucaspro"><i className="fa-brands fa-linkedin"></i> Visite meu LinkedIn.</a></li>
         </ul>
       </div>
     </div>
