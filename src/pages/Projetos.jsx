a85da68188957552141fa58fe67026945d71e7a0
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-04-27 00:03:24 -0300

    pagina PROJETOS

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Projetos = () => {
+
+  let projetos = [
+    {
+      titulo: 'Check ToDo',
+      site: '',
+      repositorio: '',
+      capa: '/assets/checktodo.png',
+      id: 'one'
+    },
+    {
+      titulo: 'Prime flick',
+      site: '',
+      repositorio: '',
+      capa: '',
+      id: 'two'
+    },
+    {
+      titulo: 'Dev Front',
+      site: '',
+      repositorio: '',
+      capa: '',
+      id: 'three'
+    },
+  ]
+
   return (
-    <div>Projetos</div>
+    <div className='container container-col'>
+      <div className='title-content'>
+        <h1>Projetos que desenvolvi.</h1>
+        <p>Websites, Aplicações web, SPA's, Sistemas interessantes.</p>
+      </div>
+
+      <div className='projects-div'>
+        {projetos.map((item, index) => {
+          return(
+            <div key={index} className='project' id={item.id}>
+              <h1>{item.titulo}</h1>
+              <div>
+                <a href="#" className='btn-ver'>Ver</a>
+                <a href="#" className='btn-repo'>Repositório</a>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
-export default Projetos
+export default Projetos;
