12dd8c47a213d772626c726e8cee0930628fcd95
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-07-21 12:07:46 -0300

    feat(design): new menu and animations

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,18 +4,28 @@ import './header.scss'
 const Header = () => {
 
   useEffect(() => {
-    function headerEffectScroll() {
-      window.addEventListener('scroll', () => {
-        if(window.scrollY > 90) {
-          document.querySelector('header').classList.add('header-active');
-        } else {
-          document.querySelector('header').classList.remove('header-active');
-        }
-      })
-    }
-
     headerEffectScroll();
+    gsapHeader();
   }, [])
+
+  function gsapHeader() {
+    const timelineHeader = gsap.timeline({ defaults: { duration: 1 }})
+    timelineHeader
+      .from('.header', { y: '-100%', ease: 'bounce' })
+      .from('.h4-header', { x: '-200%', ease: 'easeOut' })
+      .from('.links-header', { x: '100vw', ease: 'easeOut', stagger: .5 }, 1.7)
+      .from('.btn-header', { x: '200%', ease: 'easeOut' }, 2)
+  }
+
+  function headerEffectScroll() {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 90) {
+        document.querySelector('header').classList.add('header-active');
+      } else {
+        document.querySelector('header').classList.remove('header-active');
+      }
+    })
+  }
 
   function handleOpenMenu() {
     let ulElement = document.getElementById("ul");
@@ -41,13 +51,13 @@ const Header = () => {
   }
 
   return (
-    <header>
-      <h4><span>Lucas Ribeiro</span><span className='dot'>.</span></h4>
-      <button onClick={handleOpenMenu} id='btn-menu'>Menu <span id='hamb-icon'></span></button>
-      <ul id='ul'>
-        <li><a href="#home"><span>Home</span></a></li>
-        <li><a href="#about"><span>About</span></a></li>
-        <li><a href="#work"><span>Work</span></a></li>
+    <header className='header'>
+      <h4 className='h4-header'><span>Lucas Ribeiro</span><span className='dot'>.</span></h4>
+      <button onClick={handleOpenMenu} id='btn-menu' className='btn-header'>Menu <span id='hamb-icon'></span></button>
+      <ul id='ul' className='ul-header'>
+        <li className='links-header'><a href="#home"><span>Home</span></a></li>
+        <li className='links-header'><a href="#about"><span>About</span></a></li>
+        <li className='links-header'><a href="#work"><span>Work</span></a></li>
       </ul>
     </header>
   )
