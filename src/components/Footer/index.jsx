0dcad5360cfc02029ab5da216cfeae40aad64a62
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2024-01-15 14:57:04 -0300

    new website design + new color palette

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
       <p>So that we can talk more about...</p>
       <div>
         <a target='blank' href="mailto:lucasribeiro.130605@gmail.com"><i className="fa-solid fa-envelope fa-2x"></i></a>
-        <a target='blank' href="https://github.com/luckadev"><i className="fa-brands fa-github fa-2x"></i></a>
-        <a target='blank' href="https://www.linkedin.com/in/luckadev"><i className="fa-brands fa-linkedin-in fa-2x"></i></a>
-        <a target='blank' href="https://www.behance.net/luckadev"><i className="fa-brands fa-behance fa-2x"></i></a>
+        <a target='blank' href="https://github.com/luckadesign"><i className="fa-brands fa-instagram fa-2x"></i></a>
+        <a target='blank' href="https://www.linkedin.com/in/luckadesign"><i className="fa-brands fa-linkedin-in fa-2x"></i></a>
+        <a target='blank' href="https://www.behance.net/luckadesign"><i className="fa-brands fa-behance fa-2x"></i></a>
       </div>
-      <small>Made with ❣️ by <span>Lucas Ribeiro</span></small>
+      <small>Made with ❣️ by <span><a className='white-a' href="https://www.instagram.com/luckadesign">Lucka Design</a></span></small>
     </footer>
   )
 }
